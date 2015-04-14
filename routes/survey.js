319027c1ceb72124d00f82a7dca75e8d08d5162b
Brandon L. Clark <support@themindspot.com>
2015-04-14 16:16:11 -0700

    Navigation Changes

--- routes/survey.js
+++ routes/survey.js
@@ -3,41 +3,39 @@ module.exports = function (data) {
 	var router = express.Router();
 	
 	/* GET home page. */
-	router.get('/', function(req, res) {
+	/* Note: For now the following three routes can lead to the same page so long as it filters 
+	 * these sections.  If time permits try and make the page dynamic in its presentation*/
+	router.get('/', function(req, res, next) {
 		res.render('survey/index', {title:req.app.locals.service_name, user: req.session.user });
 	});
-
-	router.get('/user', function(req, res) {
-		res.render('survey/manage', {title:req.app.locals.service_name, user: req.session.user });
+	router.get('/open', function(req, res, next) {
+		res.render('survey/index', {title:req.app.locals.service_name, user: req.session.user });
 	});
-
-	router.get('/create', function(req, res) {
+	router.get('/closed', function(req, res, next) {
+		res.render('survey/index', {title:req.app.locals.service_name, user: req.session.user });
+	});
+	
+	/* GET CCA pages. */
+	router.get('/create', function(req, res, next) { // Create
 		res.render('survey/create', {title:req.app.locals.service_name, user: req.session.user });
 	});
-
-	router.get('/conduct', function(req, res) {
+	router.get('/conduct', function(req, res, next) { // Conduct
 		res.render('survey/conduct', {title:req.app.locals.service_name, user: req.session.user });
 	});
-
-	router.get('/analyze', function(req, res) {
+	router.get('/analyze', function(req, res, next) { //Analyze
 		res.render('survey/analyze', {title:req.app.locals.service_name, user: req.session.user });
 	});
 
-	router.get('/open', function(req, res) {
-		res.render('survey/index', {title:req.app.locals.service_name, user: req.session.user });
-	});
-	
-	router.get('/open/user', function(req, res) {
+	/* GET questionnaire list */	
+	router.get('/user', function(req, res, next) { // Open & Closed
+		res.render('survey/manage', {title:req.app.locals.service_name, user: req.session.user });
+	});	
+	router.get('/open/user', function(req, res, next) { // Open
 		res.render('survey/manageopen', {title:req.app.locals.service_name, user: req.session.user });
-	});
-
-	router.get('/closed', function(req, res) {
-		res.render('survey/index', {title:req.app.locals.service_name, user: req.session.user });
-	});
-	
-	router.get('/closed/user', function(req, res) {
+	});	
+	router.get('/closed/user', function(req, res, next) { // Closed
 		res.render('survey/manageclosed', {title:req.app.locals.service_name, user: req.session.user });
-	});
+	});	
 	
 	return router;
 };   
