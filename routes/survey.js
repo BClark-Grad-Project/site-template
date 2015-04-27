b6767f822abf27884120f7347e7cccb460f14ce9
Brandon L. Clark <support@themindspot.com>
2015-04-27 16:53:48 -0700

    design changes - response review - analytic results begining stages

--- routes/survey.js
+++ routes/survey.js
@@ -26,8 +26,23 @@ module.exports = function (data) {
 	router.get('/closed', function(req, res, next) {
 		res.render('survey/index', {title:req.app.locals.service_name, user: req.session.user });
 	});
+	router.get('/result/:id', function(req, res, next) { // Delete survey.
+		var id = req.params.id;
+		
+		data.survey.R.surveyForm({id:id}, function(err, survey){
+			if(err){
+				console.log(err);
+				res.redirect('/survey/analyze');
+			} else res.render('survey/result', {title:req.app.locals.service_name, user: req.session.user, survey:survey });
+		});
+	});
+
+
 	
 	/* CCA pages. */
+	/*
+	 * CREATING OPERATIONS
+	 * */
 	router.get('/create', function(req, res, next) { // GET Create
 		data.survey.read({user:req.session.user.id, state:0}, function(err, surveys){
 			console.log(err, surveys);
@@ -138,52 +153,7 @@ module.exports = function (data) {
 			} else res.redirect('/survey/create/question/' + id + '/' + question);
 		});
 	});
-
-	/*
-	 * CONDUCTING OPERATIONS
-	 * */
-	router.get('/conduct', function(req, res, next) { // GET Conduct
-		data.survey.read({user:req.session.user.id, state:1}, function(err, surveys){
-			if(err) res.redirect('/survey/conduct');
-			else res.render('survey/conduct', {title:req.app.locals.service_name, user: req.session.user, surveys:surveys });			
-		});
-	});
 	
-	router.get('/conduct/response/:id', function(req, res, next) { // GET Conduct
-		var id = req.params.id;		
-		data.survey.read({id:id, state:1}, function(err, survey){	
-			if(err) res.redirect('/survey/conduct');
-			else res.render('survey/response', {title:req.app.locals.service_name, user: req.session.user, survey: survey[0] });
-		});
-	}).post('/conduct/response/:id', function(req, res, next) {    // POST Conduct
-		res.render('survey/conduct', {title:req.app.locals.service_name, user: req.session.user });
-	});
-
-	router.get('/conduct/begin/:id', function(req, res, next) { // GET Conduct
-		var id = req.params.id;
-		data.survey.update({ id:id, state:1 }, function(err, response){
-			if(err) res.redirect('/survey/conduct');
-			else res.redirect('/survey/conduct/response/' + id);
-		});
-	});
-	router.get('/conduct/stop/:id', function(req, res, next) { // GET Conduct
-		var id = req.params.id;
-		data.survey.update({ id:id, state:0 }, function(err, response){
-			if(err) res.redirect('/survey/conduct');
-			else res.redirect('/survey/create/question/' + id);
-		});
-	});
-	router.get('/conduct/finished/:id', function(req, res, next) { // GET Conduct
-		var id = req.params.id;
-		data.survey.update({ id:id, state:3 }, function(err, response){
-			if(err) res.redirect('/survey/conduct');
-			else res.redirect('/survey/results/' + id);
-		});
-	});
-	
-	router.get('/analyze', function(req, res, next) { //Analyze
-		res.render('survey/analyze', {title:req.app.locals.service_name, user: req.session.user });
-	});
 	router.post('/update/:id', function(req, res, next) { // Delete survey.
 		var form = surveyObjs.getNewSurvey(req);
 		form.id = req.params.id;		
@@ -208,7 +178,83 @@ module.exports = function (data) {
 			} else res.redirect('/survey/create');	
 		});
 	});
+	
+	
+	
+	
 
+	/*
+	 * CONDUCTING OPERATIONS
+	 * */
+	router.get('/conduct', function(req, res, next) { // GET Conduct
+		data.survey.read({user:req.session.user.id, state:1}, function(err, surveys){
+			if(err) res.redirect('/survey/conduct');
+			else res.render('survey/conduct', {title:req.app.locals.service_name, user: req.session.user, surveys:surveys });			
+		});
+	});
+	
+	router.get('/conduct/request/:id', function(req, res, next) { // GET Conduct
+		var id = req.params.id;		
+		data.survey.read({id:id, state:1}, function(err, survey){	
+			if(err) res.redirect('/survey/conduct');
+			else res.render('survey/request', {title:req.app.locals.service_name, user: req.session.user, survey: survey[0] });
+		});
+	}).post('/conduct/request/:id', function(req, res, next) {    // POST Conduct
+		res.render('survey/conduct', {title:req.app.locals.service_name, user: req.session.user });
+	});
+
+	router.get('/conduct/begin/:id', function(req, res, next) { // GET Conduct
+		var id = req.params.id;
+		data.survey.update({ id:id, state:1 }, function(err, response){
+			if(err) res.redirect('/survey/conduct');
+			else res.redirect('/survey/conduct'); // /response/' + id);
+		});
+	});
+	router.get('/conduct/stop/:id', function(req, res, next) { // GET Conduct
+		var id = req.params.id;
+		data.survey.update({ id:id, state:0 }, function(err, response){
+			if(err) res.redirect('/survey/conduct');
+			else res.redirect('/survey/create/question/' + id);
+		});
+	});
+	router.get('/conduct/finish/:id', function(req, res, next) { // GET Conduct
+		var id = req.params.id;
+		data.survey.update({ id:id, state:2 }, function(err, response){
+			if(err) res.redirect('/survey/conduct');
+			else res.redirect('/survey/results/' + id);
+		});
+	});
+	
+	
+	
+	/*
+	 * ANALYZING OPERATIONS
+	 * */	
+	router.get('/analyze', function(req, res, next) { //Analyze
+		data.survey.read({user:req.session.user.id}, function(err, surveys){
+			if(err) res.redirect('/');
+			else res.render('survey/analyze', {title:req.app.locals.service_name, user: req.session.user, surveys:surveys });			
+		});
+	});
+	router.get('/analyze/response/:id', function(req, res, next) { // Get response documents
+		var id = req.params.id;
+		data.survey.R.responses({survey:id}, function(err, responses){
+			if(err) {
+				console.log('problem getting user responses');
+				res.redirect('/survey/sorry');
+			} else data.survey.R.surveyForm({id:id}, function(err, survey){
+				if(err) {
+					console.log('');
+					res.redirect('/survey/sorry');
+				} else res.render('survey/response', {title:req.app.locals.service_name, user: req.session.user, survey:survey, responses:responses });
+			});
+		});
+	});
+	
+	
+	
+	
+	
 	/* GET questionnaire list */	
 	router.get('/user', function(req, res, next) { // Open & Closed
 		res.render('survey/manage', {title:req.app.locals.service_name, user: req.session.user });
@@ -223,7 +269,7 @@ module.exports = function (data) {
 	router.get('/sorry', function(req, res, next) { // Closed
 		res.render('survey/sorry', {title:req.app.locals.service_name, user: req.session.user });
 	});	 
-	
+
 	router.get('/:id', function(req, res, next) { // Get survey
 		var id = req.params.id;
 		data.survey.R.surveyForm({id:id}, function(err, survey){
